a3f0495e6a20a38e6c93e1e529b2ba493f457871
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:52:49 +0200

    misc

--- src/Effect/Exception.rs
+++ src/Effect/Exception.rs
@@ -1,4 +1,4 @@
-pub type Error = crate::UnknownType;
+
 pub fn Effect_Exception_throwException() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Effect_Exception_error() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Effect_Exception_catchException() -> crate::UnknownType { crate::UnknownType::new(0) }
