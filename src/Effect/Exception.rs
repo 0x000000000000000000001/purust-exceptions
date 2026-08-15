f0e23911bedb0620d36c7d26563fe91358e52393
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:12 +0200

    misc

--- src/Effect/Exception.rs
+++
@@ -1,6 +0,0 @@
-
-pub fn Effect_Exception_throwException() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Effect_Exception_error() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Effect_Exception_catchException() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Effect_Exception_stackImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Effect_Exception_showErrorImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
